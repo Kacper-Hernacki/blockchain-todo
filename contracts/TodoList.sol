7156654bd93235514bda1b2e7f0524829a7930b3
Kacper <hernackikacper@gmail.com>
2021-08-13 14:22:57 +0200

    completed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -17,6 +17,12 @@ contract TodoList {
         bool completed
     );
 
+    event TaskCompleted(
+        uint id,
+        bool completed
+
+    );
+
     constructor() public {
         createTask('Check out The devs universe');
     }
@@ -25,6 +31,13 @@ contract TodoList {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed =  !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 
 }
--- src/app.js
+++ src/app.js
@@ -122,6 +122,13 @@ App = {
     window.location.reload()
   },
 
+  toggleCompleted: async (e) => {
+      App.setLoading(true)
+      const taskId = e.target.name
+      await App.todoList.toggleCompleted(taskId)
+      window.location.reload()
+  },
+
 
     setLoading: (boolean) => {
     App.loading = boolean
