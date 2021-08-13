d1c66a35c0fecf6d77fb2d3b741145f5493f7e3f
Kacper <hernackikacper@gmail.com>
2021-08-13 14:07:13 +0200

    create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,30 @@
 pragma solidity >=0.5.0;
 
 contract TodoList {
-uint public taskCount = 0;
+    uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    constructor() public {
+        createTask('Check out The devs universe');
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
 }
